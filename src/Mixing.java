18f613eadf8d4e91f59fdd0dc917a5f8e6f496cb
ksa <ksa@gmail.com>
2024-12-04 17:05:36 +0900

    mixing 수정

--- src/Mixing.java
+++ src/Mixing.java
@@ -2,16 +2,19 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Image;
 import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 public class Mixing extends JFrame {
@@ -76,24 +79,69 @@ public class Mixing extends JFrame {
         leftPanel2.setLayout(new BoxLayout(leftPanel2, BoxLayout.Y_AXIS));
         contentPane.add(leftPanel2, BorderLayout.WEST);
 
-        // 상단에 간격을 추가하여 버튼들이 너무 위쪽에 붙지 않게 함
-        leftPanel2.add(Box.createVerticalStrut(50));  // 위쪽 여백 추가
-
+        // 상단에 간격 추가
+        leftPanel2.add(Box.createVerticalStrut(10));  // 위쪽 여백 추가
+        
+        // 이미지 아이콘 로드
+        ImageIcon button_piano = new ImageIcon(getClass().getResource("/img/piano.png"));
+        Image img1 = button_piano.getImage();
+        Image resizedImg1 = img1.getScaledInstance(70, 70, Image.SCALE_SMOOTH);  // 크기 조정
+        button_piano = new ImageIcon(resizedImg1);
+        
         // 첫 번째 오디오 재생 버튼
-        JButton playAudio1Button = new JButton("Play Audio 1");
+        JButton playAudio1Button = new JButton(button_piano);
+        playAudio1Button.setContentAreaFilled(false);
+        playAudio1Button.setBorderPainted(false);
+        playAudio1Button.setFocusPainted(false);
+        
+        // 악기 이름
+        playAudio1Button.setText("piano");
+        playAudio1Button.setVerticalTextPosition(SwingConstants.BOTTOM); // 텍스트를 버튼의 아래쪽에 위치시키기
+        playAudio1Button.setHorizontalTextPosition(SwingConstants.CENTER); // 텍스트를 버튼의 가운데에 위치시키기
+
         playAudio1Button.addActionListener(e -> playAudio(audioFile1));
         leftPanel2.add(playAudio1Button);
 
+        
+        // 이미지 아이콘 로드
+        ImageIcon button_acoustic_guitar = new ImageIcon(getClass().getResource("/img/acousticGuitar.png"));
+        Image img2 = button_acoustic_guitar.getImage();
+        Image resizedImg2 = img2.getScaledInstance(60, 60, Image.SCALE_SMOOTH);  // 크기 조정
+        button_acoustic_guitar = new ImageIcon(resizedImg2);
+        
         // 두 번째 오디오 재생 버튼
-        JButton playAudio2Button = new JButton("Play Audio 2");
+        JButton playAudio2Button = new JButton(button_acoustic_guitar);
+        playAudio2Button.setContentAreaFilled(false);
+        playAudio2Button.setBorderPainted(false);
+        playAudio2Button.setFocusPainted(false);
+        
+        // 악기 이름
+        playAudio2Button.setText("acoustic");
+        playAudio2Button.setVerticalTextPosition(SwingConstants.BOTTOM); // 텍스트를 버튼의 아래쪽에 위치시키기
+        playAudio2Button.setHorizontalTextPosition(SwingConstants.CENTER); // 텍스트를 버튼의 가운데에 위치시키기
+
         playAudio2Button.addActionListener(e -> playAudio(audioFile2));
-        leftPanel2.add(Box.createVerticalStrut(10)); // 간격 추가
         leftPanel2.add(playAudio2Button);
 
+        
+        // 이미지 아이콘 로드
+        ImageIcon button_electric_guitar = new ImageIcon(getClass().getResource("/img/electricGuitar.png"));
+        Image img3 = button_electric_guitar.getImage();
+        Image resizedImg3 = img3.getScaledInstance(70, 70, Image.SCALE_SMOOTH);  // 크기 조정
+        button_electric_guitar = new ImageIcon(resizedImg3);
+        
         // 세 번째 오디오 재생 버튼
-        JButton playAudio3Button = new JButton("Play Audio 3");
+        JButton playAudio3Button = new JButton(button_electric_guitar);
+        playAudio3Button.setContentAreaFilled(false);
+        playAudio3Button.setBorderPainted(false);
+        playAudio3Button.setFocusPainted(false);
+        
+        // 악기 이름
+        playAudio3Button.setText("electric");
+        playAudio3Button.setVerticalTextPosition(SwingConstants.BOTTOM); // 텍스트를 버튼의 아래쪽에 위치시키기
+        playAudio3Button.setHorizontalTextPosition(SwingConstants.CENTER); // 텍스트를 버튼의 가운데에 위치시키기
+
         playAudio3Button.addActionListener(e -> playAudio(audioFile3));
-        leftPanel2.add(Box.createVerticalStrut(10)); // 간격 추가
         leftPanel2.add(playAudio3Button);
     }
 
